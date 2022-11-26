8c4a424653bac352a9ee8ad72f24694c9085d111
prashtomer <prash.tomer@gmail.com>
2022-11-26 11:33:56 +0530

    fetch orders from database

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -25,6 +25,36 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
+  Future<void> fetchAndSetOrders() async {
+    final url = Uri.parse(
+        'https://tomer-flutter-shop-4af3a-default-rtdb.firebaseio.com/orders.json');
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if(extractedData == null) {
+      return;
+    }
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(OrderItem(
+        id: orderId,
+        amount: orderData['amount'],
+        dateTime: DateTime.parse(orderData['dateTime']),
+        products: (orderData['products'] as List<dynamic>)
+            .map(
+              (item) => CartItem(
+                id: item['id'],
+                price: item['price'],
+                quantity: item['quantity'],
+                title: item['title'],
+              ),
+            )
+            .toList(),
+      ));
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(
     List<CartItem> cartProducts,
     double total,
@@ -38,12 +68,14 @@ class Orders with ChangeNotifier {
         {
           'amount': total,
           'dateTime': timeStamp.toIso8601String(),
-          'products': cartProducts.map((cp) => {
-            'id': cp.id,
-            'title': cp.title,
-            'quantity': cp.quantity,
-            'price': cp.price,
-          }).toList(),
+          'products': cartProducts
+              .map((cp) => {
+                    'id': cp.id,
+                    'title': cp.title,
+                    'quantity': cp.quantity,
+                    'price': cp.price,
+                  })
+              .toList(),
         },
       ),
     );
