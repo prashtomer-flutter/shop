232dde75bc36096ca84c295a0eced335e5e4ef90
prashtomer <prash.tomer@gmail.com>
2022-11-26 14:33:22 +0530

    add error handling in login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import './providers/auth.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 import './providers/products.dart';
+import './screens/auth_screen.dart';
 import './screens/cart_screen.dart';
+import './screens/edit_product_screen.dart';
 import './screens/orders_screen.dart';
 import './screens/product_detail_screen.dart';
-import './screens/products_overview_screen.dart';
 import './screens/user_products_screen.dart';
-import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (ctx) => Auth()),
         ChangeNotifierProvider(create: (ctx) => Products()), // create is used instead of value because new instance of Products is getting created. refer value usage in products_grid.dart
         ChangeNotifierProvider(create: (ctx) => Cart()), // same as above
         ChangeNotifierProvider(create: (ctx) => Orders()) // same as above
@@ -32,7 +34,7 @@ class MyApp extends StatelessWidget {
           ),
           fontFamily: 'Lato',
         ),
-        home: ProductsOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
---
+++ lib/providers/auth.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import '../models/http_exception.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _expiryDate;
+  String _userId;
+
+  Future<void> _authenticate(
+    String email,
+    String password,
+    String urlSegment,
+  ) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:${urlSegment}?key=[API_KEY]'); // API_KEY can be found from project settings in firebase
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if(responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch(error) {
+      throw error;
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+}
